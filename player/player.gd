5513936bc50a6d6624d5abd308208055bb62c77f
Nekohime Musou <nekohime.musou@gmail.com>
2023-12-27 19:59:02 -0500

    Step 15: Player Knockback

--- player/player.gd
+++ player/player.gd
@@ -9,6 +9,8 @@ signal healthChanged
 @export var maxHealth = 3
 @onready var currentHealth: int = maxHealth
 
+@export var knockbackPower = 500
+
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection * speed
@@ -25,16 +27,9 @@ func updateAnimation():
 
 		animations.play("walk" + direction)
 
-func handleCollision():
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		var collider = collision.get_collider()
-		print_debug(collider.name)
-
 func _physics_process(_delta):
 	handleInput()
 	move_and_slide()
-	handleCollision()
 	updateAnimation()
 
 
@@ -43,4 +38,11 @@ func _on_hurt_box_area_entered(area):
 		currentHealth -= 1
 		if currentHealth < 0:
 			currentHealth = maxHealth
+
 		healthChanged.emit(currentHealth)
+		knockback(area.get_parent().velocity)
+
+func knockback(enemyVelocity: Vector2):
+	var knockbackDirection = -(enemyVelocity + velocity).normalized() * knockbackPower
+	velocity = knockbackDirection
+	move_and_slide()
