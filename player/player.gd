f104a04352720390aacb36c55f0e2a82b673785b
Nekohime Musou <nekohime.musou@gmail.com>
2023-12-29 20:39:02 -0500

    Melee Attack Part 1: Animation

--- player/player.gd
+++ player/player.gd
@@ -7,6 +7,7 @@ signal healthChanged
 @onready var effects = $Effects
 @onready var hurtBox = $hurtBox
 @onready var hurtTimer = $hurtTimer
+@onready var weapon = $weapon
 
 @export var maxHealth = 3
 @onready var currentHealth: int = maxHealth
@@ -15,7 +16,10 @@ signal healthChanged
 
 @export var inventory: Inventory
 
+var lastAnimDirection: String = "Down"
+
 var isHurt: bool = false
+var isAttacking: bool = false
 
 func _ready():
 	effects.play("RESET")
@@ -24,7 +28,20 @@ func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection * speed
 	
+	if Input.is_action_just_pressed("attack"):
+		attack()
+
+func attack():
+	animations.play("attack" + lastAnimDirection)
+	isAttacking = true
+	weapon.visible = true
+	await animations.animation_finished
+	isAttacking = false
+	weapon.visible = false
+
 func updateAnimation():
+	if isAttacking: return
+	
 	if velocity.length() == 0:
 		if animations.is_playing():
 			animations.stop()
@@ -35,6 +52,7 @@ func updateAnimation():
 		elif velocity.y < 0: direction = "Up"
 
 		animations.play("walk" + direction)
+		lastAnimDirection = direction
 
 func _physics_process(_delta):
 	handleInput()
