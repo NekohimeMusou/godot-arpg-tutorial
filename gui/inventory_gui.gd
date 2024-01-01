d78e763ef1f0d6eb62c9435ef6daa0d9ce086a22
Nekohime Musou <nekohime.musou@gmail.com>
2023-12-31 21:22:23 -0500

    Inventory: Right Click to Return Item

--- gui/inventory_gui.gd
+++ gui/inventory_gui.gd
@@ -10,6 +10,8 @@ var isOpen: bool = false
 @onready var slots: Array = $NinePatchRect/GridContainer.get_children()
 
 var itemInHand: ItemStackGui
+var oldIndex: int = -1
+var locked: bool = false
 
 func _ready():
 	connectSlots()
@@ -50,6 +52,8 @@ func close():
 	closed.emit()
 
 func onSlotClicked(slot):
+	if locked: return
+	
 	if slot.isEmpty():
 		if !itemInHand: return
 		
@@ -70,6 +74,8 @@ func takeItemFromSlot(slot):
 	itemInHand = slot.takeItem()
 	add_child(itemInHand)
 	updateItemInHand()
+	
+	oldIndex = slot.index
 
 func stackItems(slot):
 	var slotItem: ItemStackGui = slot.itemStackGui
@@ -84,6 +90,7 @@ func stackItems(slot):
 		slotItem.inventorySlot.amount = totalAmount
 		remove_child(itemInHand)
 		itemInHand = null
+		oldIndex = -1
 	else:
 		slotItem.inventorySlot.amount = maxAmount
 		itemInHand.inventorySlot.amount = totalAmount - maxAmount
@@ -98,6 +105,8 @@ func insertItemInSlot(slot):
 	remove_child(itemInHand)
 	itemInHand = null
 	slot.insert(item)
+	
+	oldIndex = -1
 
 func swapItems(slot):
 	var tempItem = slot.takeItem()
@@ -112,5 +121,26 @@ func updateItemInHand():
 	if !itemInHand: return
 	itemInHand.global_position = get_global_mouse_position() - itemInHand.size / 2
 
+func putItemBack():
+	locked = true
+	if oldIndex < 0:
+		var emptySlots = slots.filter(func (s): return s.isEmpty())
+		if emptySlots.is_empty(): return
+		oldIndex = emptySlots[0].index
+	
+	var targetSlot = slots[oldIndex]
+	
+	var tween = create_tween()
+	var targetPosition = targetSlot.global_position + targetSlot.size / 2
+	tween.tween_property(itemInHand, "global_position", targetPosition, 0.2)
+	
+	await tween.finished
+	
+	insertItemInSlot(targetSlot)
+	locked = false
+
 func _input(_event):
+	if itemInHand && !locked && Input.is_action_just_pressed("rightClick"):
+		putItemBack()
+	
 	updateItemInHand()
